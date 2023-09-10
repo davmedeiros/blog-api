f687529995d0d5871298d44825443ba8884a0a54
David Medeiros <123705989+davmedeiros@users.noreply.github.com>
2023-09-10 12:11:15 +0100

    Add user routes

--- routes/router.js
+++ routes/router.js
@@ -1,10 +1,29 @@
 const express = require('express');
 const router = express.Router();
 
+// Home
 router.get('/', (req, res) => {
   res.send(`GET ${req.url}  - to be implemented...`);
 });
 
+// User
+router.get('/user/:id', (req, res) => {
+  res.send(`GET ${req.url} - to be implemented...`);
+});
+
+router.post('/user/:id', (req, res) => {
+  res.send(`POST ${req.url} - to be implemented...`);
+});
+
+router.put('/user/:id', (req, res) => {
+  res.send(`PUT ${req.url} - to be implemented...`);
+});
+
+router.delete('/user/:id', (req, res) => {
+  res.send(`DELETE ${req.url} - to be implemented...`);
+});
+
+// Post
 router.get('/post/:id', (req, res) => {
   res.send(`GET ${req.url} - to be implemented...`);
 });
