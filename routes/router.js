31c2ecd4313d0d53d9d7fa9fd208073a6347251c
David Medeiros <123705989+davmedeiros@users.noreply.github.com>
2023-09-10 12:05:59 +0100

    Create home and post routes

--- routes/router.js
+++ routes/router.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  res.send('Express');
+router.get('/', (req, res) => {
+  res.send(`GET ${req.url}  - to be implemented...`);
+});
+
+router.get('/post/:id', (req, res) => {
+  res.send(`GET ${req.url} - to be implemented...`);
+});
+
+router.post('/post/:id', (req, res) => {
+  res.send(`POST ${req.url} - to be implemented...`);
+});
+
+router.put('/post/:id', (req, res) => {
+  res.send(`PUT ${req.url} - to be implemented...`);
+});
+
+router.delete('/post/:id', (req, res) => {
+  res.send(`DELETE ${req.url} - to be implemented...`);
 });
 
 module.exports = router;
