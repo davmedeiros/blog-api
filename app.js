f122bd5d2700b3022798fa451e51434f7a1cca29
David Medeiros <123705989+davmedeiros@users.noreply.github.com>
2023-09-10 12:31:53 +0100

    Replace deprecated send(status) call

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use((err, req, res, next) => {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
   res.status(status);
-  res.send(status);
+  res.sendStatus(status);
 });
 
 module.exports = app;
